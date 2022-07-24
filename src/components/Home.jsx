ebe42a5bf6c760792bfd5081773bfe2c7034cd98
Evan Kim <evan977512@gmail.com>
2022-07-23 23:09:15 -0600

    7 commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,10 +9,7 @@ const Home = () => {
         <p className="text-[#e6c4ae]">Hi, This is</p>
         <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">Evan Kim</h1>
         <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">I'm a Full Stack Devloper.</h2>
-        <p className="text-[#8892b0] py-4 max-w-[700px]">
-          Yet remarkably appearance get him his projection. Diverted endeavor bed peculiar men the not desirous. Acuteness abilities ask can offending furnished fulfilled sex. Warrant fifteen exposed ye at mistake. Blush since so in noisy still built up an again. As young ye hopes no he place means. Partiality diminution gay yet entreaties admiration. In mr it he mention perhaps attempt pointed
-          suppose. Unknown ye chamber of warrant of norland arrived.
-        </p>
+        <p className="text-[#8892b0] py-4 max-w-[700px]">Welcome!!. I'm a Full Stack Developer. I'm a passionate person who loves to create things. I'm a self-taught developer who is always learning new things. I'm a hard worker who is always trying to be the best.</p>
 
         <div>
           <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600 ">

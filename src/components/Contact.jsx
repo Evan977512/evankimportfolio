6580450a03d7e9ae04152c0cc8a5c9a037c1038f
EvanKim <youngchan.kim@edu.sait.ca>
2022-08-15 17:14:32 -0600

    yarn add yarn

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
       <form method="POST" action="https://getform.io/f/0e09dfcb-0756-447d-86ef-8bdeca7e3a5c" className="flex flex-col max-w-[600px] w-full">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300">Contact</p>
-          <p className="text-gray-300 py-4">// Submit the form below or shoot me an email - evan977512@gmail.com</p>
+          <p className="text-gray-300 py-4"> Submit the form below or shoot me an email - evan977512@gmail.com</p>
         </div>
 
         <input className="bg-[#ccd6f6] p-2" type="text" placeholder="Name" name="name" />

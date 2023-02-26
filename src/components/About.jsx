a5a65017dd275bfb1059bb9ecef20d5b48993cc6
Evan Kim <evan977512@gmail.com>
2023-02-26 00:50:19 -0700

    updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,7 +12,7 @@ const About = () => {
         </div>
         <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
           <div className="sm:text-right text-4xl font-bold">
-            <p>Hi, I'am Evan Kim, nice to see you : Please take a round.</p>
+            <p>Hi, I'am Evan Kim, nice to see you : Please take a round</p>
           </div>
           <div>
             <p>It's pleasure to see you. Welcome to my Portfolio website!</p>

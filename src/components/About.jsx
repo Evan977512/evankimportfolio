68fa83c9fd0a2d908114dd82fa03c786c3bd7690
EvanKim <evan977512@gmail.com>
2023-06-06 17:34:11 -0600

    spell missing corrected.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,38 +1,40 @@
-import React from 'react';
+import React from "react";
 
 const About = () => {
-    return (
-        <div name="about" className="w-full h-screen bg-[#081324] text-gray-300">
-            <div className="flex flex-col justify-center items-center w-full h-full">
-                <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
-                    <div className="sm:text-right pb-8 pl-4">
-                        <p className="text-4xl font-bold inline border-b-4 border-pink-600">About</p>
-                    </div>
-                </div>
-                <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
-                    <div className="sm:text-right text-4xl font-bold">
-                        <p>Hi, I'am Evan Kim, nice to see you : Please take a round</p>
-                    </div>
-                    <div>
-                        <p>
-                            It's a pleasure to see you. Welcome to my Portfolio website! My name is Evan Kim and I'm a Junior Full Stack developer. I
-                            have a passion for learning new technologies and applying them to create innovative solutions. Currently, I am a student
-                            at SAIT and I'm eager to find a new job in the local city.
-                        </p>
-                        <p>
-                            I strongly believe in the power of cooperation and teamwork to achieve great results. Throughout my academic and
-                            professional experience, I have honed my skills in collaboration, communication, and problem-solving. I am excited to use
-                            my skills to make a positive impact in the industry and bring value to any team I work with.
-                        </p>
-                        <p>
-                            Thank you for taking the time to visit my portfolio. Please feel free to explore and reach out if you have any questions
-                            or opportunities to collaborate.
-                        </p>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div name="about" className="w-full h-screen bg-[#081324] text-gray-300">
+      <div className="flex flex-col justify-center items-center w-full h-full">
+        <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
+          <div className="sm:text-right pb-8 pl-4">
+            <p className="text-4xl font-bold inline border-b-4 border-pink-600">About</p>
+          </div>
         </div>
-    );
+        <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
+          <div className="sm:text-right text-4xl font-bold">
+            <p>Hi, I'm Evan Kim, nice to meet you :)</p>
+          </div>
+          <div>
+            <p>
+              It's a pleasure to have you here. Welcome to my portfolio website! My name is Evan Kim, and I'm a Junior
+              Full Stack developer. I have recently completed my studies at SAIT and I'm now actively seeking new job
+              opportunities in the local area. I have a strong passion for learning new technologies and applying them
+              to create innovative solutions.
+            </p>
+            <p>
+              I firmly believe in the power of cooperation and teamwork to achieve outstanding results. Throughout my
+              academic and professional journey, I have developed valuable skills in collaboration, communication, and
+              problem-solving. I am excited to utilize these skills to make a positive impact in the industry and
+              contribute value to any team I join.
+            </p>
+            <p>
+              Thank you for taking the time to explore my portfolio. Please feel free to navigate through it and don't
+              hesitate to reach out if you have any questions or potential collaboration opportunities.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,8 +8,11 @@ const Home = () => {
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
         <p className="text-[#e6c4ae]">Hi, This is</p>
         <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">Evan Kim</h1>
-        <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">I'm a Full Stack Devloper.</h2>
-        <p className="text-[#8892b0] py-4 max-w-[700px]">Welcome!!. I'm a Full Stack Developer. I'm a passionate person who loves to create things. I'm a self-taught developer who is always learning new things. I'm a hard worker who is always trying to be the best.</p>
+        <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">I'm a Full Stack Developer.</h2>
+        <p className="text-[#8892b0] py-4 max-w-[700px]">
+          Welcome! I'm a Full Stack Developer. I'm a passionate person who loves to create things. I'm a self-taught
+          developer who is always learning new things. I'm a hard worker who is always trying to be the best.
+        </p>
 
         <div>
           <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600 ">
